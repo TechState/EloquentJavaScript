6f2cd44b2ecf27424bb1b43234d32181a9a8f749
Alexey Nikolaev <mailboxn@mail.ru>
2019-08-20 17:25:37 +0400

    Continue programming library

--- libraries/objects generator.js
+++ libraries/objects generator.js
@@ -1,20 +1,32 @@
-let randomObjInstantiate = function(depth) {
-    let propNumber = getRandomNumber();
+let txt = require(`./chunk`);
+let str = 'Widget with id';
+
+let getRndObj = function(depth) {
+    let propNumber = getRndNum();
 }
 
-let getRandomValue = function() {
+let getRndVal = function() {
     let value = null;
-    let type = (Math.random() < 4.5) ? 0 : 1;
+    let type = (Math.random() <= 5) ? 0 : 1;
 
-    if (type) return getRandomNumber();
+    if (type) return getRndNum(getRndNum(5));
 
-    for (let i = 0; i < getRandomNumber(); i++) {
-        
+    for (let i = 0; i < getRndNum(2); i++) {
+
     }
 
 }
 
-let getRandomNumber = function() {
-    return new Math.round(Math.random() * 100
-    + Math.random() * 10 + Math.random());
+let getRndNum = function(digit) {
+    return Math.round(Math.random() * Math.pow(10, digit));
 }
+
+let getSentence = function(position, txt) {
+    let lastPosition = null;
+    position = txt.indexOf('.', position);
+    lastPosition = txt.indexOf('.', position + 1);
+    return txt.slice(position + 1, lastPosition);
+}
+
+console.log(typeof(txt));
+console.log(getSentence(1800, txt));
